9fe0cbd7231d3aa3438f2b32a239de121d60cd23
Nitin <nk17oct@gmail.com>
2024-08-25 20:36:01 +0530

    add counter info page

--- app/orderInfo.tsx
+++ app/orderInfo.tsx
@@ -1,12 +1,82 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { Colors } from "@/constants/Colors";
+import { useNavigation } from "expo-router";
+import OrderBS from "@/components/bottom_sheet/OrderBS";
+import CBBottomSheet from "@/components/CBBottomSheet";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
 
 export default function orderInfo() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({ title: "Meal O Pedia" });
+  }, [navigation]);
+
   return (
-    <View>
-      <Text>orderInfo</Text>
-    </View>
+    <ScrollView>
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <View style={{ height: 200 }}></View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({});
+interface ItemCardProps {
+  item?: {
+    name: string;
+    amount: number;
+  };
+}
+
+const snapPoints = ["25%", "50%"];
+
+function ItemCard({ item }: ItemCardProps) {
+  const bsref = useRef<BottomSheetModal>(null);
+  return (
+    <Pressable
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: 16,
+        paddingVertical: 24,
+        marginHorizontal: 32,
+        borderBottomWidth: 1,
+        borderBottomColor: "rgb(50, 50, 50)",
+        gap: 32,
+        // flex: 1,
+      }}
+      onPress={() => bsref.current?.present()}
+    >
+      <Text style={[styles.itemCardText, { flex: 1, flexWrap: "wrap" }]}>
+        Veg Buffet
+      </Text>
+      <Text style={[styles.itemCardText, { fontWeight: "600", fontSize: 14 }]}>
+        Rs 100.00
+      </Text>
+
+      <CBBottomSheet ref={bsref} snapPoints={snapPoints}>
+        <OrderBS />
+      </CBBottomSheet>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  itemCardText: {
+    color: Colors.dark.text,
+    fontSize: 16,
+    fontWeight: "400",
+  },
+});
--- components/CounterCard.tsx
+++ components/CounterCard.tsx
@@ -1,16 +1,19 @@
 import { Colors } from "@/constants/Colors";
 import { StyleSheet, Text, View, Image } from "react-native";
 import ReactLogo from "@/assets/images/react-logo.png";
+import { Link } from "expo-router";
 
 export default function CounterCard() {
   return (
-    <View style={styles.box}>
-      <Image source={ReactLogo} style={{ height: 50, width: 50 }} />
-      <View style={{ marginLeft: 12 }}>
-        <Text style={styles.name}>Meal O Pedia</Text>
-        <Text style={styles.order}>2 pending requests</Text>
+    <Link href="/orderInfo">
+      <View style={styles.box}>
+        <Image source={ReactLogo} style={{ height: 50, width: 50 }} />
+        <View style={{ marginLeft: 12 }}>
+          <Text style={styles.name}>Meal O Pedia</Text>
+          <Text style={styles.order}>2 pending requests</Text>
+        </View>
       </View>
-    </View>
+    </Link>
   );
 }
 

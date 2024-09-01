65466a8ecd8dfda5d0c9efc1b8adedbf9ad8fe66
Nitin <nk17oct@gmail.com>
2024-09-02 01:44:06 +0530

    remove unnecessary files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -45,7 +45,7 @@ export default function TabLayout() {
           ),
         }}
       />
-      <Tabs.Screen
+      {/* <Tabs.Screen
         name="explore"
         options={{
           title: "Explore",
@@ -56,7 +56,7 @@ export default function TabLayout() {
             />
           ),
         }}
-      />
+      /> */}
       <Tabs.Screen
         name="settings"
         options={{

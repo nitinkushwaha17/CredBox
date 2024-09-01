a78519dc2b81851cc2a07dc13fabd8b3daa5d108
Nitin <nk17oct@gmail.com>
2024-09-01 19:05:34 +0530

    completed order card theme

--- components/CompletedOrderCard.tsx
+++ components/CompletedOrderCard.tsx
@@ -4,16 +4,18 @@ import CBBottomSheet from "./CBBottomSheet";
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
 import { useRef } from "react";
 import MyOrderBS from "./bottom_sheet/MyOrderBS";
+import { useStyle } from "@/hooks/useStyle";
 
 const snapPoints = ["25%", "50%"];
 
 export default function CompletedOrderCard() {
+  const styles = useStyle(style);
   const bsref = useRef<BottomSheetModal>(null);
   return (
     <Pressable style={styles.box} onPress={() => bsref.current?.present()}>
       <View style={{ flex: 1, justifyContent: "space-between" }}>
         <View>
-          <Text style={[styles.counterText, { fontSize: 8 }]}>2 hrs ago</Text>
+          {/* <Text style={[styles.counterText, { fontSize: 8 }]}>2 hrs ago</Text> */}
           <Text style={styles.counterText}>Meal O Pedia</Text>
           <Text style={styles.itemText}>Watermelon juice</Text>
         </View>
@@ -34,36 +36,37 @@ export default function CompletedOrderCard() {
   );
 }
 
-const styles = StyleSheet.create({
-  box: {
-    height: 120,
-    width: 120,
-    backgroundColor: Colors.dark.background,
-    padding: 12,
-    borderRadius: 8,
-  },
-  counterText: {
-    fontSize: 12,
-    color: Colors.dark.icon,
-    fontWeight: "600",
-  },
-  itemText: {
-    fontSize: 16,
-    fontWeight: "600",
-    color: Colors.dark.text,
-  },
-  amountText: {
-    fontSize: 12,
-    fontWeight: "600",
-    color: Colors.dark.text,
-  },
-  icon: {
-    color: Colors.dark.icon,
-    fontSize: 18,
-    borderColor: Colors.dark.icon,
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingLeft: 2,
-    paddingTop: 2,
-  },
-});
+const style = (Colors: any) =>
+  StyleSheet.create({
+    box: {
+      height: 120,
+      width: 120,
+      backgroundColor: Colors.onBackground,
+      padding: 12,
+      borderRadius: 8,
+    },
+    counterText: {
+      fontSize: 12,
+      color: Colors.icon,
+      fontWeight: "600",
+    },
+    itemText: {
+      fontSize: 16,
+      fontWeight: "600",
+      color: Colors.text,
+    },
+    amountText: {
+      fontSize: 12,
+      fontWeight: "600",
+      color: Colors.text,
+    },
+    icon: {
+      color: Colors.icon,
+      fontSize: 18,
+      borderColor: Colors.icon,
+      borderWidth: 1,
+      borderRadius: 5,
+      paddingLeft: 2,
+      paddingTop: 2,
+    },
+  });

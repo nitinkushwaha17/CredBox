77e21a0c66851c02b8d59752650f8cd5acaa2955
Nitin <nk17oct@gmail.com>
2025-05-17 16:49:54 +0530

    server url

--- axios/index.ts
+++ axios/index.ts
@@ -1,10 +1,14 @@
+import { isDevEnv } from "@/utils";
 import axios from "axios";
 import Constants from "expo-constants";
 
+const isInDevEnv = isDevEnv();
+
 export default axios.create({
-  baseURL:
-    `http://${Constants.expoConfig?.hostUri
-      ?.split(":")
-      .shift()
-      ?.concat(":3000")}` ?? "http://192.168.0.105:3000",
+  baseURL: isInDevEnv
+    ? `http://${Constants.expoConfig?.hostUri
+        ?.split(":")
+        .shift()
+        ?.concat(":3000")}` ?? "http://192.168.0.105:3000"
+    : "https://credbox-server.onrender.com",
 });
